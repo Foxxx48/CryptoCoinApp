dcd8780d9d539a06fab0abb8a4cfa63c389e50eb
Foxxx48 <Foxxdim@mail.ru>
2022-09-29 16:06:53 +0300

    delete notifyDataSetChange and use DiffUtilCallback

--- app/src/main/java/com/fox/cryptocoinapp/presentation/adapter/CoinInfoAdapter.kt
+++ app/src/main/java/com/fox/cryptocoinapp/presentation/adapter/CoinInfoAdapter.kt
@@ -3,36 +3,27 @@ package com.fox.cryptocoinapp.presentation.adapter
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.ListAdapter
 import com.fox.cryptocoinapp.R
-import com.fox.cryptocoinapp.data.mapper.CoinMapper.Companion.BASE_IMAGE_URL
 import com.fox.cryptocoinapp.databinding.ItemCoinInfoBinding
 import com.fox.cryptocoinapp.domain.CoinInfo
 import com.squareup.picasso.Picasso
 
-class CoinInfoAdapter(private val context: Context) :
-    RecyclerView.Adapter<CoinInfoViewHolder>() {
-
-    var coinInfoList: List<CoinInfo> = listOf()
-        set(value) {
-            field = value
-            notifyDataSetChanged()
-        }
+class CoinInfoAdapter(
+    private val context: Context
+) : ListAdapter<CoinInfo, CoinInfoViewHolder>(CoinInfoDiffCallback) {
 
     var onCoinClickListener: OnCoinClickListener? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CoinInfoViewHolder {
-        val binding = ItemCoinInfoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding =
+            ItemCoinInfoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return CoinInfoViewHolder(binding)
-//        val view =
-//            LayoutInflater.from(parent.context).inflate(R.layout.item_coin_info, parent, false)
-//        return CoinInfoViewHolder(view)
+
     }
 
-    override fun getItemCount() = coinInfoList.size
-
     override fun onBindViewHolder(holder: CoinInfoViewHolder, position: Int) {
-        val coin = coinInfoList[position]
+        val coin = getItem(position)
         with(holder.binding) {
             with(coin) {
                 val symbolsTemplate = context.resources.getString(R.string.symbols_template)
